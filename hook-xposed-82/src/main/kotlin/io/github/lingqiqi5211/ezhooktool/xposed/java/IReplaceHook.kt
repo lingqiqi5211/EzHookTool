029a7da91910456e39e32fe271bf097e4a92b30b
柒柒喵 <lingqiqi233@gmail.com>
2026-05-16 12:04:43 +0800

    refactor: Change IMethodHook and IReplaceHook to interfaces, and add ExtraFields helper methods

--- hook-xposed-82/src/main/kotlin/io/github/lingqiqi5211/ezhooktool/xposed/java/IReplaceHook.kt
+++ hook-xposed-82/src/main/kotlin/io/github/lingqiqi5211/ezhooktool/xposed/java/IReplaceHook.kt
@@ -5,12 +5,12 @@ import io.github.lingqiqi5211.ezhooktool.xposed.common.HookParam
 /**
  * 供 Java 实现的 replace hook 回调。
  */
-abstract class IReplaceHook {
+fun interface IReplaceHook {
     /**
      * 替换原方法执行结果。
      *
      * @param param 当前 hook 调用参数
      * @return 返回给原调用方的结果
      */
-    abstract fun replace(param: HookParam): Any?
+    fun replace(param: HookParam): Any?
 }
